5fff09bfbcda76023eb45aea7590066cf5bd72ae
Philip Damra <djheru@gmail.com>
2018-05-23 22:25:30 -0400

    Tweak service worker caching globPattern

--- course-project-starting-setup/workbox-config.js
+++ course-project-starting-setup/workbox-config.js
@@ -1,7 +1,12 @@
 module.exports = {
   "globDirectory": "public/",
   "globPatterns": [
-    "**/*.{html,ico,json,css,png,jpg,js}"
+    "**/*.{html,ico,json,css,js}",
+    "src/images/*.{jpg,png}"
   ],
-  "swDest": "public/service-worker.js"
+  "swDest": "public/service-worker.js",
+  "globIgnores": [
+    "../workbox-config.js",
+    "help/**"
+  ]
 };
